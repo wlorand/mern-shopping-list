2617e7b98b813026973762bdfd1e54f1c7936812
Will Lorand <bill@milemarker3.org>
2018-11-25 20:14:14 -0800

    temp allow config/ into git

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+/**
+ * File: config/keys.js
+ * Desc: store config details here: DB credentials, API keys etc
+ *        && do not put this file under version control (see .gitignore)
+ */
+
+ module.exports = {
+    mongoURI: 'mongodb://will:will123@ds133202.mlab.com:33202/mern_shopping_list_db'
+ }
